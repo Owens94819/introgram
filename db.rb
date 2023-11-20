6b367006e39c7cd58475a3dceb9adba83de8c51d
owens <65927654+Owens94819@users.noreply.github.com>
2023-11-20 02:07:37 +0100

    ...99

--- db.rb
+++ db.rb
@@ -1,19 +1,30 @@
 require 'mysql2'
-# Connect to MySQL
-client = Mysql2::Client.new(
-  host: 'localhost',
-  username: 'root',
-  password: '',
-  database: 'localstorage'
-)
+require 'yaml'
+require 'active_record'
+puts 88
 
-# Execute a simple query
-results = client.query('SELECT * FROM your_table')
+# Load database configuration from YAML file in the config directory
+db_config = YAML.load_file('config/database.yml')
+puts 88
 
-# Process the query results
-results.each do |row|
-  puts "Column1: #{row['column1']}, Column2: #{row['column2']}"
-end
+# Connect to the database
+ActiveRecord::Base.establish_connection(db_config['development'])
+puts 88
+# # Connect to MySQL
+# client = Mysql2::Client.new(
+#   host: 'localhost',
+#   username: 'root',
+#   password: '',
+#   database: 'localstorage'
+# )
 
-# Close the connection
-client.close
+# # Execute a simple query
+# results = client.query('SELECT * FROM your_table')
+
+# # Process the query results
+# results.each do |row|
+#   puts "Column1: #{row['column1']}, Column2: #{row['column2']}"
+# end
+
+# # Close the connection
+# client.close
